8b5936d7ce0be98257f03ddb74f6dc7f477e54a6
soubhagya-kumar-behera <soubhagyabehera074@gmail.com>
2026-08-24 22:43:31 +0530

    chore: initialize FlashReserve backend

---
+++ backend/src/main/java/com/soubhagya/flashreserve/FlashreserveApplication.java
@@ -0,0 +1,13 @@
+package com.soubhagya.flashreserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashreserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashreserveApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/soubhagya/flashreserve/FlashreserveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soubhagya.flashreserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashreserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
